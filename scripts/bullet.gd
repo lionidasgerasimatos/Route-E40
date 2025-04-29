5efe5abdd512f10fcf3261eae3170a9456eaa30f
lionidasgerasimatos <lgerasimatos@gmail.com>
2025-04-29 17:08:10 +0200

    Fixed bullet add muzzle flash and fire rate

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,5 +3,9 @@ extends Area2D
 @export var BULLET_SPEED = 2
 var direction: Vector2
 
+
+	
+	
 func _physics_process(delta: float) -> void:
+	rotation =direction.angle()
 	global_position += direction * BULLET_SPEED
--- scripts/gun.gd
+++ scripts/gun.gd
@@ -3,17 +3,41 @@ extends CharacterBody2D
 var bullet_scene = preload("res://Scene/bullet.tscn")
 @onready var  barrel_pos = $barrel
 @onready var shooting_time:= $shooting_time
+@onready var muzzle_flash:= $barrel_light
 
+var light_timer :=0.0
+var light_duration := 0.1  # How long the light should stay visible (in seconds)
+
+
+
+
+func _ready():
+	print(muzzle_flash) #test
 
 func _process(delta: float) -> void:
-	look_at(get_global_mouse_position())
+	look_at(get_global_mouse_position()) # Following the mouse with in-build godot function 
 	
-	if Input.is_action_pressed("leftMshoot") and shooting_time.is_stopped():
+	#Lighting
+	if light_timer > 0 :   #While the limer is active the muzzle flash happends
+		light_timer -= delta
+		muzzle_flash.visible =true 
+		muzzle_flash.energy = lerp(2.0,0.0,1.0 - (light_timer/ light_duration)) # dynamic on and off 
+	else :
+		muzzle_flash.visible =false
+	
+	#Shooting
+	if Input.is_action_pressed("leftMshoot") and shooting_time.is_stopped(): 
 		var bullet = bullet_scene.instantiate()
 		bullet.global_position = barrel_pos.global_position
-		get_tree().get_current_scene().add_child(bullet)
-		bullet.direction = (get_global_mouse_position()-global_position).normalized()
-		$shooting_time.start()
+		get_tree().get_current_scene().add_child(bullet) #Must add as a child otherwise it wont work at spawn
 		
+		# The .normalized is because godot coordinates are fliped -1 is 1 and reverse 
+		bullet.direction = (get_global_mouse_position()-global_position).normalized() 
+		$barrel_light.visible =true
+		$shooting_time.start() #begin the Fire Rate 
+		light_timer = light_duration 
+		muzzle_flash.energy = 2.0  # Start muzzle flash with strong light
+		muzzle_flash.visible = true #Turn on the flash 
+
 	
 	
