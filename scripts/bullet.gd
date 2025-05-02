60162bdc1a8f2cc054158a634276a4e3517d238a
lionidasgerasimatos <lgerasimatos@gmail.com>
2025-05-02 20:58:06 +0200

    \

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,8 +1,19 @@
 extends Area2D
 
+@onready var  _animator := $AnimationPlayer
+
 @export var BULLET_SPEED = 2
 var direction: Vector2
+
+
+
+
 
 func _physics_process(delta: float) -> void:
 	rotation =direction.angle()
 	global_position += direction * BULLET_SPEED
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	_animator.play("Hit")
