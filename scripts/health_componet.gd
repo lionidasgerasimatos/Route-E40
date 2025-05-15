3ae6e016c1bbd270bd3be621d1a023fe1a24f0aa
lionidasgerasimatos <lgerasimatos@gmail.com>
2025-05-15 16:39:17 +0200

    Add Health Component

---
+++ scripts/health_componet.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name  HealthComponent
+
+
+@export var Max_Health:= 100
+var health : float
+
+
+
+func _ready() -> void:
+	health= Max_Health
+	
+
+func damage(attack: Attack):
+	health -= attack.attack_damage
+	
+	if health <= 0:
+		queue_free()	
