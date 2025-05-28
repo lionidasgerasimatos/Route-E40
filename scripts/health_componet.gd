50bf183c05ad7ca1b240f85a3cc87ecd9d6e7c6a
lionidasgerasimatos <lgerasimatos@gmail.com>
2025-05-28 21:36:04 +0200

    Fix children deletion after death

--- scripts/health_componet.gd
+++ scripts/health_componet.gd
@@ -1,16 +1,14 @@
 extends Node2D
 class_name  HealthComponent
 
-
 @export var Max_Health:= 100
+
 var health : float
 signal health_changed
-
 
 func _ready() -> void:
 	health= Max_Health
 	
-
 func damage(attack: Attack):
 	health -= attack.attack_damage
 	health_changed.emit()
